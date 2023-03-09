882a3cc491c330e24e5514a3bec9311efe0edec0
Ankit Kumar <emankitz029@gmail.com>
2023-03-09 13:16:54 +0530

    first commit for java

---
+++ first.java
@@ -0,0 +1,27 @@
+public class java_all_methods {
+//it is build on the basis of logics that's cool;
+    static int logic (int x,int y ){
+        int z;
+     if(x>y){
+         z=x+y;
+     }   else {
+         z=(x+y)*5;
+     }
+     return z ;
+    }
+
+    public static void main(String[] args) {
+
+
+    int c;
+    int a=2;
+        int b=23;
+        c=logic(a,b);
+
+        System.out.println(c);
+
+
+
+
+    }
+}
