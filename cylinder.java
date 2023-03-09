86f32ccd4cee33f73c742b9eff0b281b4618073e
Ankit Kumar <emankitz029@gmail.com>
2023-03-09 13:58:43 +0530

    initial changes

---
+++ cylinder.java
@@ -0,0 +1,36 @@
+//class cylinder {
+////    private  int height;
+//private int radius;
+//private  String name="wow bro";
+//    public int getRadius() {
+//        return radius;
+//    }
+//
+//    public void setRadius(int radius) {
+//        this.radius = radius;
+//    }
+////public void setHeight(int h){
+////    this.height=h;
+////}public  int getheight(int c){
+////    return c;
+////    }
+////public void  setRadius(int r){
+////    this.radius=r;
+////}
+////public int getRadius(int t){
+////    return t;
+////}}
+//
+//public class cylinder_height_width {
+//    public static void main(String[] args) {
+//cylinder cool =new cylinder();
+//// cool.setRadius (23);
+//int wo =cool.getRadius();
+//
+//        System.out.println(String(wow.name));
+//    System.out.println();
+//        System.out.println(wo);
+//
+//
+//    }
+//}}
