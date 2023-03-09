dabe65114b5bda4565c86c37e4b6f347724d1653
Ankit Kumar <emankitz029@gmail.com>
2023-03-09 13:50:15 +0530

    finding days

---
+++ week_day_to_find.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class week_day_to_find {
+    public static void main(String[] args){
+//        Scanner src =new Scanner(System.in);
+        int day;
+        System.out.println("enter your number:");
+Scanner st=new Scanner(System.in);
+day =st.nextInt();
+
+
+
+        switch (day){
+            case 1:
+//                System.out.println("enter your number");
+//                int dww= src.nextInt();
+//                System.out.println("enter your number(");
+               System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
