1b872b240173fa7a2e6ccd389715bcdf4e2119be
Ankit Kumar <emankitz029@gmail.com>
2023-03-09 14:15:34 +0530

    initial commit

---
+++ fast.java
@@ -0,0 +1,18 @@
+//
+import java.util.Scanner;
+public class fast { public static void main(String[]args){
+    System.out.println("Taking input from the user");
+  Scanner sc =new Scanner(System.in);
+//        System.out.println("Enter number1");
+//        int a= sc.nextInt();
+//        boolean c2= sc.hasNext();
+//        System.out.println(c2);
+//        System.out.println("Enter number2");
+//    int b= sc.nextInt();
+//    int sum =a-b;
+//        System.out.println(sum);
+
+        String str=sc.nextLine();
+        System.out.println(str);
+    }
+}
